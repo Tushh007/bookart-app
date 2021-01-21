c0645a378691585fbc95fb8b245720942cd23bde
Tushar Langer <tushar.langer@gmail.com>
2021-01-21 19:19:38 +0530

    deployment

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/bookart',
+    database: 'mongodb+srv://bookartadmin:KUh15357FTAYSYVa@bookartapp.mrnpk.mongodb.net/bookartapp?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
 
